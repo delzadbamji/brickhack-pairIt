ca724696207ab97b914af6b0125c98220c656bdd
delzadbamji <delzadbamji@gmail.com>
2022-03-05 19:38:14 -0500

    create constants and page layouts

---
+++ pair-it/src/constants/constants.js
@@ -0,0 +1,251 @@
+let randomNumberTo10 = Math.floor(Math.random() * 10);
+
+const alcohols = [
+  "Martini",
+  "Rum",
+  "Whisky",
+  "Baileys Irish Cream",
+  "Apple Cider",
+  "Sambuca",
+  "Tequila",
+  "Wine",
+  "Beer",
+  "Vodka"
+];
+
+let questionList = [
+  {
+    // Q1
+    question: "What food would you choose?",
+    answers: [
+      {
+        content: "Pizza",
+        value: ""
+      },
+      {
+        content: "Sandwich",
+        value: ""
+      },
+      {
+        content: "Veggies",
+        value: ""
+      }
+    ]
+  },
+  {
+    // Q2
+    question: "What is your Favourite Game?",
+    answers: [
+      {
+        content: "Polo",
+        value: ""
+      },
+      {
+        content: "Football",
+        value: ""
+      },
+      {
+        content: "Video Games",
+        value: ""
+      }
+    ]
+  },
+  {
+    // Q3
+    question: "How do you describe yourself?",
+    answers: [
+      {
+        content: "Smart",
+        value: ""
+      },
+      {
+        content: "adventurous",
+        value: ""
+      },
+      {
+        content: "introverted",
+        value: ""
+      }
+    ]
+  },
+  {
+    // Q4
+    question: "Choose a number.",
+    answers: [
+      {
+        content: randomNumberTo10,
+        value: ""
+      },
+      {
+        content: randomNumberTo10,
+        value: ""
+      },
+      {
+        content: randomNumberTo10,
+        value: ""
+      }
+    ]
+  },
+  {
+    // Q5
+    question: "What would be your hobby?",
+    answers: [
+      {
+        content: "Reading",
+        value: ""
+      },
+      {
+        content: "Scuba Diving",
+        value: ""
+      },
+      {
+        content: "dancing",
+        value: ""
+      }
+    ]
+  },
+  {
+    // Q6
+    question: "What is your spirit animal",
+    answers: [
+      {
+        content: "wolf",
+        value: ""
+      },
+      {
+        content: "deer",
+        value: ""
+      },
+      {
+        content: "horse",
+        value: ""
+      }
+    ]
+  },
+  {
+    // Q7
+    question: "choose a pet",
+    answers: [
+      {
+        content: "cat",
+        value: ""
+      },
+      {
+        content: "dog",
+        value: ""
+      },
+      {
+        content: "lizard",
+        value: ""
+      }
+    ]
+  },
+  {
+    // Q8
+    question: "Choose a superhero",
+    answers: [
+      {
+        content: "ironman",
+        value: ""
+      },
+      {
+        content: "superman",
+        value: ""
+      },
+      {
+        content: "groot",
+        value: ""
+      }
+    ]
+  },
+  {
+    // Q9
+    question: "What is your ideal place to live",
+    answers: [
+      {
+        content: "tent",
+        value: ""
+      },
+      {
+        content: "apartment",
+        value: ""
+      },
+      {
+        content: "house",
+        value: ""
+      }
+    ]
+  },
+  {
+    // Q10
+    question: "What is your choice of transport?",
+    answers: [
+      {
+        content: "motorbike",
+        value: ""
+      },
+      {
+        content: "car",
+        value: ""
+      },
+      {
+        content: "bicycle",
+        value: ""
+      }
+    ]
+  },
+  {
+    // Q11
+    question: "Which department would you fit best in?",
+    answers: [
+      {
+        content: "army",
+        value: ""
+      },
+      {
+        content: "navy",
+        value: ""
+      },
+      {
+        content: "air force",
+        value: ""
+      }
+    ]
+  },
+  {
+    // Q12
+    question: "What fruit would you choose",
+    answers: [
+      {
+        content: "apple",
+        value: ""
+      },
+      {
+        content: "pineapple",
+        value: ""
+      },
+      {
+        content: "watermelon",
+        value: ""
+      }
+    ]
+  },
+  {
+    // Q13
+    question: "What is important to you?",
+    answers: [
+      {
+        content: "inner beauty",
+        value: ""
+      },
+      {
+        content: "outer beauty",
+        value: ""
+      },
+      {
+        content: "intelligence",
+        value: ""
+      }
+    ]
+  }
+];
