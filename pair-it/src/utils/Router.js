7de2697d6f4f8b010a5a0fc414a8af74f672d651
shweta <shwetawahane97@gmail.com>
2022-03-05 20:45:14 -0500

    Created LandingPage and navigation routes.

---
+++ pair-it/src/utils/Router.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "../views/LandingPage";
+import QuizPage from "../views/QuizPage";
+
+const Router = (props) => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route render={() => <h1>404: page not found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
